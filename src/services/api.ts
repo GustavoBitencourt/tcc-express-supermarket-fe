f0ee8b64289174641e5d8bb4a36ea2592522f676
Gustavo_Bitencourt <gustavo.bitencourt@compasso.com.br>
2023-08-16 11:45:47 -0300

    Add connection with backend
    add connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+import { CustomerData } from '../interfaces/CustomerData'
+import { Snack } from '../interfaces/Snack'
 import { SnackData } from '../interfaces/SnackData'
 
 const api = axios.create({
@@ -10,5 +12,31 @@ export const getBurgers = () => api.get<SnackData[]>('/snacks?snack=burger')
 export const getPizzas = () => api.get<SnackData[]>('/snacks?snack=pizza')
 export const getDrinks = () => api.get<SnackData[]>('/snacks?snack=drink')
 export const getIceCreams = () => api.get<SnackData[]>('/snacks?snack=ice-cream')
+
+export const processCheckout = (cart: Snack[], customer: CustomerData) =>
+  api.post('/checkout', {
+    cart,
+    customer: {
+      fullName: customer.fullName,
+      email: customer.email,
+      mobile: customer.mobile,
+      document: customer.document,
+      zipCode: customer.zipCode,
+      street: customer.street,
+      number: customer.number,
+      complement: customer.complement,
+      neighborhood: customer.neighborhood,
+      city: customer.city,
+      state: customer.state,
+    },
+    payment: {
+      creditCardNumber: customer.creditCardNumber,
+      creditCardHolder: customer.creditCardHolder,
+      creditCardExpiration: `${new Date(customer.creditCardExpiration).getMonth() + 1}/${new Date(
+        customer.creditCardExpiration,
+      ).getFullYear()}`,
+      creditCardSecurityCode: customer.creditCardSecurityCode,
+    },
+  })
 
 export default api
