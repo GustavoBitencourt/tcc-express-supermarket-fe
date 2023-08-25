1ac912840d83b097ca648f08ddadd89d343a87f3
Gustavo_Bitencourt <gustavo.bitencourt@compasso.com.br>
2023-08-25 13:35:31 -0300

    Update button empyCar
    update button

--- src/components/EmptyCart/index.tsx
+++ src/components/EmptyCart/index.tsx
@@ -10,7 +10,7 @@ export function EmptyCart({ title }: EmptyCartProps) {
   return (
     <Container>
       <h2>{title}</h2>
-      <Button to='/'>Checar o cardápio</Button>
+      <Button to='/'>Checar o catálogo</Button>
       <img src={manAndLimpezaImg} alt='Homem com hambúrguer' />
     </Container>
   )
