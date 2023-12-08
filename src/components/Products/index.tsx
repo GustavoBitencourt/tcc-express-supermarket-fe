5c03965ca016ac4b787aacdd719cbedfc3490ee5
Gustavo_Bitencourt <gustavo.bitencourt@compasso.com.br>
2023-12-08 14:26:34 -0300

    Add function modal product
    add modal product

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'
+import Modal from 'react-modal'
 import { currencyFormat } from '../../helpers/currencyFormat'
 import { useCart } from '../../hooks/useCart'
 import { ProductData } from '../../interfaces/ProductData'
@@ -5,17 +7,27 @@ import { SkeletonProduct } from './SkeletonProduct'
 import { Container } from './styles'
 import { ReactComponent as PlusIcon } from '../../assets/plus-icon.svg'
 import { ReactComponent as MapIcon } from '../../assets/map-product.svg'
+import { ReactComponent as ImageMap } from '../../assets/image-map.svg'
 
 interface ProductsProps {
   products: ProductData[]
 }
 
+Modal.setAppElement('#root')
+
 export function Products({ products }: ProductsProps) {
   const { cart, addProductIntoCart } = useCart()
+  const [isMapModalOpen, setMapModalOpen] = useState(false)
+  const [selectedProduct, setSelectedProduct] = useState<ProductData | null>(null)
 
   const openMapModal = (product: ProductData) => {
-    // Implemente a lógica para mostrar o modal ou realizar a ação do mapa aqui
-    console.log('Abrir modal do mapa para:', product.name)
+    setSelectedProduct(product)
+    setMapModalOpen(true)
+  }
+
+  const closeMapModal = () => {
+    setSelectedProduct(null)
+    setMapModalOpen(false)
   }
 
   return (
@@ -51,6 +63,85 @@ export function Products({ products }: ProductsProps) {
               </div>
             )
           })}
+      <Modal
+        isOpen={isMapModalOpen}
+        onRequestClose={closeMapModal}
+        contentLabel='Map Modal'
+        style={{
+          overlay: { zIndex: 100 },
+          content: {
+            top: '0',
+            left: '0',
+            right: '0',
+            bottom: '0',
+            border: 'none',
+            padding: 0,
+            background: 'transparent',
+          },
+        }}
+      >
+        {selectedProduct && (
+          <div style={{ position: 'relative', display: 'flex', flexDirection: 'column' }}>
+            <div
+              style={{
+                background: '#FFF',
+                borderRadius: '8px',
+                boxShadow: '0px 8px 24px 0px rgba(112, 144, 176, 0.20)',
+                display: 'flex',
+                alignItems: 'center',
+                width: '90%',
+                position: 'absolute',
+                marginTop: '5rem',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+              }}
+            >
+              <img
+                src={selectedProduct.image}
+                alt={selectedProduct.name}
+                style={{ width: '98px', height: '86px', marginRight: '20px' }}
+              />
+              <div>
+                <h3
+                  style={{
+                    color: '#393939',
+                    fontFamily: 'Manrope',
+                    fontSize: '16px',
+                    fontStyle: 'normal',
+                    fontWeight: 700,
+                    textTransform: 'uppercase',
+                  }}
+                >
+                  {selectedProduct.name}
+                </h3>
+              </div>
+            </div>
+            <ImageMap style={{ width: '100vw', height: '100vh', marginTop: '-2.5rem' }} />
+            {/* Adicione aqui quaisquer informações adicionais do produto se necessário */}
+            <button
+              type='button'
+              onClick={closeMapModal}
+              style={{
+                position: 'absolute',
+                bottom: '3%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: '80%',
+                height: '3.2rem',
+                backgroundColor: '#56BA50',
+                color: 'white',
+                padding: '10px',
+                border: 'none',
+                borderRadius: '8px',
+                zIndex: 2000,
+                fontSize: '16px',
+              }}
+            >
+              Confirmar
+            </button>
+          </div>
+        )}
+      </Modal>
     </Container>
   )
 }
