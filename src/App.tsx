d5a2abda955e7427cf8021fa394f8613b3ece20a
Gustavo_Bitencourt <gustavo.bitencourt@compasso.com.br>
2023-12-08 17:16:30 -0300

    Fix routes
    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const queryClient = new QueryClient()
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename='/'>
+      <BrowserRouter basename={process.env.REACT_APP_BASE_NAME}>
         <Theme>
           <ProductProvider>
             <CartProvider>
